81781327558ae574df2708530cfbe2eb75c40c2d
stacief510 <staciemfraser@gmail.com>
2018-03-21 21:53:24 -0700

    final touches and styles to game

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 console.log('Ready to Race!');
 $(document).ready(function(){
 
-
-
  $(document).on('keydown', function(event){
 let player1Location = $('.player1').css('left').split('px');
 let player1LocNum = parseInt(player1Location);
@@ -12,20 +10,33 @@ let player2LocNum = parseInt(player2Location);
 
 
 if (event.keyCode === 90) {
-    if (player1LocNum < 400) {
+    if (player1LocNum < 1000) {
         $('.player1').animate({ left: '+=50px' });
     } 
+	else if (player1LocNum >= 1000){
+    	$('player1').stop();
+	}
 }
 	else if (event.keyCode === 77) {
-    	if (player2LocNum < 400) {
+    	if (player2LocNum < 1000) {
         $('.player2').animate({ left: '+=50px' });
+    } else if (player2LocNum >= 1000){
+    	$('player2').stop();
     }
 } 
-if (player1LocNum >= 400 || player2LocNum >= 400) {
+if (player1LocNum >= 1000 || player2LocNum >= 1000) {
         alert("You win!");
     }
 
 });
 
+
+ function reset(event) {
+   $('.player1').animate({'right':'','left': '0px'});
+   $('.player2').animate({'right':'','left': '0px'});
+   playerOneMoves = 0;
+   playerTwoMoves = 0;
+ }
+$('button').on('click', reset);
 
 }); //end of all JS.
